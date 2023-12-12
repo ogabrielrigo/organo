d4a6deb2e7d7f267d6f1a162d3c169d04f6b28c1
GabrielRigoAlmeida <gabriel.rigo@extrafruti.com.br>
2023-12-12 07:05:21 -0300

    criação dos hook's + funcionalidade de adicionar colaborador

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,10 +1,11 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
+    
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio}>
+            <select onChange={e => props.aoAlterado(e.target.value)} required={props.obrigatorio} value={props.valor}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
